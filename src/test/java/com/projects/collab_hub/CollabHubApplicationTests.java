56001d9565a17dba9c648c910b1583ac07022908
MSreekari <msreekari11@gmail.com>
2025-07-14 20:54:12 +0530

    Initial files

---
+++ src/test/java/com/projects/collab_hub/CollabHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.collab_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
